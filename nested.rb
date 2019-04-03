b3366ab31efbc3591e0b4dabf13be754d983ea65
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-04-03 05:19:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,25 @@ def hopper
           :languages => ["C"]
         }
      }
-
+		 
+		 def hopper
+		 	programmer_hash = 
+		  		{
+		         :grace_hopper => {
+		           :known_for => "COBOL",
+		           :languages => ["COBOL", "FORTRAN"]
+		         },
+		         :alan_kay => {
+		           :known_for => "Object Orientation",
+		           :languages => ["Smalltalk", "LISP"]
+		         },
+		         :dennis_ritchie => {
+		           :known_for => "Unix",
+		           :languages => ["C"]
+		         }
+		      }
+		 puts 
+		 programmer_hash["grace_hopper"]
 
 end
 
